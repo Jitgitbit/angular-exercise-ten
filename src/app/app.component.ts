03d94037a7b2350a305dfeddc6f88d27baa5e7f1
Jitgitbit <rowowolf@gmail.com>
2020-05-13 10:02:33 +0200

    Creating Custom Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,10 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+
+  forbiddenUsernames = ['Cris', 'Cristina', 'Maria'];
   genders = ['male', 'female'];
+
   signupForm: FormGroup;
 
   get controls() {
@@ -17,7 +20,7 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),  // Yes the good'old JS trick to binding for exceptions !!!
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -30,9 +33,15 @@ export class AppComponent implements OnInit{
   }
 
   onAddHobby(){
-
     const control = new FormControl(null, Validators.required);
 
     (<FormArray>this.signupForm.get('hobbies')).push(control);           // this is uncommon ! with FormArray you have to explicitly cast !
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){         // this "-1" is needed for checking presence of control.value in array !
+      return {'nameIsForbidden': true};                               // because "-1" returns as true !
+    }
+    return null;
   }
 }
